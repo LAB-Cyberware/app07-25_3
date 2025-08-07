be3da486fec51235fd251591ccda0c33665a75f1
LAB-Cyberware <lab.cyberware@gmail.com>
2025-08-07 13:06:03 -0400

    imgia

--- app/page.js
+++ app/page.js
@@ -4,38 +4,31 @@ import { useSession, signIn, signOut } from 'next-auth/react' /* Importaciones d
 signOut de Next Auth. */
 import { useRouter } from 'next/navigation' /* Importación de useRouter de Next. */
 import { useEffect } from 'react' /* Importación de useEffect de React. */
+import { CldImage } from 'next-cloudinary';
 
-export default function Login() {   /* La función de login, con toda la configuración dentro. */
-  const { data: session, status } = useSession() /* Hook que hace que tanto session como
-  status sean extraídos de useSession. Además, renombra la data a session. Aclarar que session 
-  representa la información del usuario logueado, mientras que status el estado de la autenticación, como puede ser
-  un "Cargando...". */
-  const router = useRouter() /* Obtiene el objeto useRouter y lo asigna como router. Se usa para 
-  navegación. */
+export default function Login() {  
+  const { data: session, status } = useSession() 
+  const router = useRouter() 
 
-  useEffect(() => { /* Configuración del useEffect, este se ejecuta cuando cambian las dependencias. */ 
-    if (session && session.user.rol === 'admin') { /* Si la sesión existe y el rol del usuario es
-      "admin". */
-      router.push('/admin') /* Envía a la carpeta admin y toma el page.js (página). */
+  useEffect(() => { 
+    if (session && session.user.rol === 'admin') {
+      router.push('/admin')
     }
-  }, [session, router]) /* Arreglo que indica que el useEffect solo se ejecute cuando cambian las
-  dependencias. */
+  }, [session, router])
 
-  if (status === 'loading') return <p>Cargando...</p> /* Si el status es "loading" retornara el mensaje
-  de "Cargando...". El "loading" proviene de Next Auth. */
+  if (status === 'loading') return <p>Cargando...</p> 
 
   
-  if (session) { /* Si la sesión está iniciada. */
-    if (session.user.rol === 'admin') { /* Si la sesión está iniciada, pero si el rol del usuario en la
-      sesión es 'admin'. */
-      return ( /* Si es 'admin' retornará lo siguiente. */
+  if (session) { 
+    if (session.user.rol === 'admin') {
+      return ( 
         <div className="p-6 max-w-md mx-auto bg-white rounded-lg shadow-md"> 
           <h1 className="text-2xl font-bold mb-4">Cargando...</h1> 
           <p>Cargando sesión de Administrador...</p>
         </div> /* Mensaje de sesión de Administrador. */
       )
     }
-    return ( /* Si la sesión es como usuario o mod. */
+    return ( 
       <div className="p-6 max-w-md mx-auto bg-white rounded-lg shadow-md">
         <h1 className="text-2xl font-bold mb-4">Bienvenido</h1>
         <div className="space-y-2">
@@ -48,8 +41,17 @@ export default function Login() {   /* La función de login, con toda la configu
             }`}> {session.user.rol}
             </span>
           </p>
+          <CldImage
+            src="cld-sample-5" // Use this sample image or upload your own via the Media Explorer
+            width="500" // Transform the image: auto-crop to square aspect_ratio
+            height="500"
+            crop={{
+              type: 'auto',
+              source: true
+            }}
+          />
         </div>
-        <button /* Botón de cierre de sesión del respectivo usuario. Activa el signOut de NextAuth. */
+        <button 
           onClick={() => signOut()} 
           className="mt-4 w-full bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded"
         >
@@ -59,11 +61,10 @@ export default function Login() {   /* La función de login, con toda la configu
     )
   }
 
-  return ( /* Si se cierra la sesión entonces pondrá disponible el inicio de sesión. */
+  return ( 
     <div className="p-6 max-w-md mx-auto bg-white rounded-lg shadow-md">
       <h1 className="text-2xl font-bold mb-4">Iniciar sesión</h1>
-      <button /* Botón de inicio de sesión. Activa el signIn de NextAuth y que sea a través de
-      'google'. */
+      <button 
         onClick={() => signIn('google')}
         className="w-full bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
       >
